77123861cdcf0afec5353a735108c92a289ca0f2
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2019-10-02 23:51:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "457566403793518603") return;
+  if (message.author.id !== "483033057872576541") return;
 
   
   if (message.content.startsWith(prefix + 'watch')) {
